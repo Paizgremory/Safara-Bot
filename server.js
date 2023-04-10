b314b342d458d4261ddec47cecb5544b106330c4
Paizgremory <105426888+Paizgremory@users.noreply.github.com>
2023-04-10 22:41:22 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,350 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on('ready', async () => {
+  console.log(`${client.user.tag} sudah online!`)
+  client.user.setActivity("Jangan Terlalu Berharap Kepada Seseorang, Apalagi Ngeharepin Aku >~<")
+})
+
+client.on('message', async message => {
+  if(message.content === "kontol") message.reply("Jangan Toxic Gitu Dong Banh :c")
+})
+client.on('message', async message => {
+  if(message.content === "KONTOL") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Kontol") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "memek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Memek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "MEMEK") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "ANJING") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Anjing") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "anjing") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "anj") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "salam") message.reply("Assalamualaikum Mas Bukan Salam Gitu")
+})
+client.on('message', async message => {
+  if(message.content === "Salam") message.reply("Assalamualaikum Mas Bukan Salam Gitu")
+})
+client.on('message', async message => {
+  if(message.content === "SALAM") message.reply("Assalamualaikum Mas Bukan Salam Gitu")
+})
+client.on('message', async message => {
+  if(message.content === "celek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Celek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "CELEK") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "ASW") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "asw") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "ASU") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Asu") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "asu") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Assalamualaikum") message.reply("Waalaikumsalam")
+})
+client.on('message', async message => {
+  if(message.content === "p") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "P") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "pe") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "pa") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "PE") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "PA") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "Pa") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "Pe") message.reply("Salam Dong Jangan pa pe pa pe Mulu :c")
+})
+client.on('message', async message => {
+  if(message.content === "Pepek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Ppk") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "PEPEK") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "pepek") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Ngentod") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "ngentod") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "NGENTOD") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "Bacot") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "bacot") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "BACOT") message.reply("Jangan Toxic Gitu Dong :c")
+})
+client.on('message', async message => {
+  if(message.content === "MIN") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "Min") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "min") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "capek euy") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "Capek euy") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "Capek Euy") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "CAPEK EUY") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "Safara Cantik") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "safara cantik") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "SAFARA CANTIK") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "CANTIK DEH SAFARA") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "cantik deh safara") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "Cantik deh safara") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "Cantik Deh Safara") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "Safara") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "safara") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "SAFARA") message.reply("Iya Kenapa?")
+})
+client.on('message', async message => {
+  if(message.content === "Safara Cantik Deh") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "Safara cantik deh") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "safara cantik deh") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "SAFARA CANTIK DEH") message.reply("Makasih Yaah:heart_eyes:")
+})
+client.on('message', async message => {
+  if(message.content === "capek") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "Capek") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "CAPEK") message.reply("Semangat Yah:smiling_face_with_3_hearts:")
+})
+client.on('message', async message => {
+  if(message.content === "Paizz") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "Paiz") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "paizz") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "paiz") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "Pais") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "pais") message.reply("Apa Lu Manggil Ayank Gua:face_with_symbols_over_mouth:")
+})
+client.on('message', async message => {
+  if(message.content === "information") message.reply("SAYA SELAKU FOUNDER DI SERVER FANTASY PRIDE INGIN MEMBUKA DONASI BUAT MEMBER YANG PENGEN DONASI SE IHKLAS NYA BUAT BELI RDP BIAR SERVER GA STUCK LOGIN LAGI, DENGAN DONASI 10K AJA UDAH CUKUP BUAT ON IN SERVER JADI NORMAL LAGI, BUAT YANG IHKLAS AJA KARENA LAGI KRISIS JUGA EKONOMI GUA YAK :v YANG MAU DONASI BISA DM Paizz YAK. VIA : DANA ONLY YAK @everyone")
+})
+client.on('message', async message => {
+  if(message.content === "ip") message.reply("<#1026442632156688385> Awas Ae Lu Sebar, Kalo Lu Sebar Gua Getek Pala Lu!")
+})
+client.on('message', async message => {
+  if(message.content === "IP") message.reply("<#1026442632156688385> Awas Ae Lu Sebar, Kalo Lu Sebar Gua Getek Pala Lu!")
+})
+client.on('message', async message => {
+  if(message.content === "Ip") message.reply("<#1026442632156688385> Awas Ae Lu Sebar, Kalo Lu Sebar Gua Getek Pala Lu!")
+})
+client.on('message', async message => {
+  if(message.content === "iP") message.reply("<#1026442632156688385> Awas Ae Lu Sebar, Kalo Lu Sebar Gua Getek Pala Lu!") 
+})
+client.on('message', async message => {
+  if(message.content === "Hallo") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "Hello") message.reply("Hello Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "Halo") message.reply("Halo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "hallo") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "halo") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "hello") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "helo") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "HELLO") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "HELO") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "HALLO") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "HALO") message.reply("Hallo Jugaa")
+})
+client.on('message', async message => {
+  if(message.content === "SEPI") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPPI") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Sepi") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPI NIH") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPPI NIHH") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPPI NIH") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Seppi") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Sepi nihh") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Sepi Nih") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Seppi nihh") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPIii") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "SEPIII") message.reply("Iya Nih Ramein Dong")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu Nanyak?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu Nanya?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu Nanyak") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu nanyak?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu nanya?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu nanyak") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu nanya") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "Kamu nanya?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "kamu nanya") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "kamu nanyak") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "kamu nanya?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "KAMU NANYA?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "KAMU NANYAK?") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "KAMU NANYA") message.reply("Afaantuhhh??")
+})
+client.on('message', async message => {
+  if(message.content === "KAMU NANYAK") message.reply("Afaantuhhh??")
+})
+
+
+client.login("MTAwOTc3MDM1NjcyODQwMTk3MA.G97TUA.0RaV3huHr7vBxIEOge6qpv7497Pk0beIVEJu8c") 
